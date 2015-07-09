040d4510176bcfccbbfe52300b2012f52a0e845b
galeno <lenghan1991@gmail.com>
2015-07-09 23:55:26 +0800

    Initial Commit

---
+++ tabat-ios/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  tabat-ios
+//
+//  Created by 涵冷 on 7/9/15.
+//  Copyright (c) 2015 涵冷. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
